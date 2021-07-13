368b6c65b00696c23a67feabb6965537ed79f5f7
freddysundowner <githumbi3fred@gmail.com>
2021-07-13 14:00:38 +0300

    v

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
-const http = require('http');
 var express = require('express');
 var app = express();
 
-const hostname = '127.0.0.1';
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
 
 function generateTwKey(){
     const AccessToken = require('twilio').jwt.AccessToken;
